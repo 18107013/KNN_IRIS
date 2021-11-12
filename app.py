4709be4f93ff950d77b3ce89950e87b52ebc5fed
18107013 <78843374+18107013@users.noreply.github.com>
2021-11-11 23:54:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,26 +6,54 @@ import pandas as pd
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectFromModel
 from sklearn.model_selection import train_test_split,cross_validate
+import joblib
 
 st.title("Malware Classification")
 data = pd.read_csv("train_set_label.csv",header=0)
 df = data.drop(["e_magic","e_crlc","e_res","e_res2"],axis=1)
 labels = df["class"].values
 
-df_new = df[["LoaderFlags","NumberOfSymbols","BaseOfCode","MajorImageVersion","MinorImageVersion" ,"CheckSum"]]
-x_train,x_test,y_train,y_test = train_test_split(df_new,labels,test_size=0.2,random_state=101)
-model = RandomForestClassifier(n_estimators=50)
-model.fit(x_train,y_train)
+x_vif = df[["e_lfanew","NumberOfSections","CreationYear","PointerToSymbolTable","NumberOfSymbols","Characteristics","MajorLinkerVersion",
+            "MinorLinkerVersion","ImageBase","SectionAlignment","FileAlignment","MajorOperatingSystemVersion","MinorOperatingSystemVersion","MajorImageVersion",
+           "MinorImageVersion","MajorSubsystemVersion","MinorSubsystemVersion","SizeOfHeaders","CheckSum","Subsystem","DllCharacteristics","SizeOfStackReserve","SizeOfStackCommit",
+            "SizeOfHeapReserve","SizeOfHeapCommit","LoaderFlags"]]
+model = joblib.load("malware_detection_VIF")
 
 
-xmin = np.min(df_new,axis=0)#min values in the datasets
-xmax = np.max(df_new,axis=0)#max values in the datasets
-LoaderFlags = st.slider('LoaderFlags',float(xmin[0]),float(xmax[0]))
-NumberOfSymbols = st.slider('NumberOfSymbols',float(xmin[1]),float(xmax[1]))
-BaseOfCode = st.slider('BaseOfCode',float(xmin[2]),float(xmax[2]))
-MajorImageVersion= st.slider('MajorImageVersion',float(xmin[3]),float(xmax[3]))
-MinorImageVersion= st.slider('MinorImageVersion',float(xmin[4]),float(xmax[4]))
-CheckSum = st.slider('CheckSum',float(xmin[5]),float(xmax[5]))
-y_pred = model.predict([[LoaderFlags,NumberOfSymbols,BaseOfCode,MajorImageVersion,MinorImageVersion,CheckSum]])
-op = ["No Malware","Malware Detected"]
+
+xmin = np.min(x_vif,axis=0)#min values in the datasets
+xmax = np.max(x_vif,axis=0)#max values in the datasets
+e_lfanew = st.number_input('e_lfanew',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+NumberOfSections = st.number_input('NumberOfSections',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+CreationYear = st.number_input('CreationYear',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+PointerToSymbolTable = st.number_input('PointerToSymbolTable',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+NumberOfSymbols = st.number_input('NumberOfSymbols',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+Characteristics = st.number_input('Characteristics',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+MajorLinkerVersion = st.number_input('MajorLinkerVersion',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+MinorLinkerVersion = st.number_input('MinorLinkerVersion',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+ImageBase = st.number_input('ImageBase',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+SectionAlignment = st.number_input('SectionAlignment',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+FileAlignment = st.number_input('FileAlignment',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+MajorOperatingSystemVersion = st.number_input('"MajorOperatingSystemVersion',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+MinorOperatingSystemVersion = st.number_input('MinorOperatingSystemVersion',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+MajorImageVersion= st.number_input('MajorImageVersion',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+MinorImageVersion= st.number_input('MinorImageVersion',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+MajorSubsystemVersion= st.number_input('MajorSubsystemVersion',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+MinorSubsystemVersion= st.number_input('MinorSubsystemVersion',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+SizeOfHeaders= st.number_input('SizeOfHeaders',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+CheckSum = st.number_input('CheckSum',min_value=xmin[0],max_value=xmax[0],value=60601,step=1)
+Subsystem = st.number_input('Subsystem',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+DllCharacteristics = st.number_input('DllCharacteristics',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+SizeOfStackReserve= st.number_input('SizeOfStackReserve',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+SizeOfStackCommit= st.number_input('SizeOfStackCommit',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+SizeOfHeapReserve= st.number_input('SizeOfStackReserve',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+SizeOfHeapCommit= st.number_input('SizeOfStackReserve',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+LoaderFlags = st.number_input('LoaderFlags',min_value=xmin[0],max_value=xmax[0],value=50,step=1)
+
+#MinorImageVersion= st.slider('MinorImageVersion',float(xmin[4]),float(xmax[4]))
+y_pred = model.predict([[e_lfanew,NumberOfSections,CreationYear,PointerToSymbolTable,NumberOfSymbols,Characteristics,MajorLinkerVersion,
+            MinorLinkerVersion,ImageBase,SectionAlignment,FileAlignment,MajorOperatingSystemVersion,MinorOperatingSystemVersion,MajorImageVersion,
+           MinorImageVersion,MajorSubsystemVersion,MinorSubsystemVersion,SizeOfHeaders,CheckSum,Subsystem,DllCharacteristics,SizeOfStackReserve,SizeOfStackCommit,
+            SizeOfHeapReserve,SizeOfHeapCommit,LoaderFlags]])
+op = ["no Malware","Malware Detected"]
 st.title(op[y_pred[0]])
